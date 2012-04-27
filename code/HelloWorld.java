1c2c8672ea4570b4dc0754d579cf89f673dfcfe7
Adam Witwer <adam@oreilly.com>
2012-04-27 16:32:21 -0400

    more updates to chapter 2

---
+++ code/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.marakana;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloWorld extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
