a9955c56ccd9595bf03347da19232039803ad206
Jessica Hosman <jhosman@touchstone.east.ora.com>
2012-12-11 16:21:04 -0500

    More misc updates to code section

--- code/HelloWorld.java
+++ code/HelloWorld.java
@@ -3,11 +3,11 @@ package com.marakana;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class HelloWorld extends Activity {
+public class HelloWorld extends Activity {  <1>
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);  <2>
 		setContentView(R.layout.main);
 	}
 }
